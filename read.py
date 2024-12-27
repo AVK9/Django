b2f95e5d5c0005809b0eddde2cfc72a7a634c64e
AlexKat <katsubo79@gmail.com>
2024-12-27 16:29:01 +0200

    2

--- read.py
+++ read.py
@@ -1,12 +1,31 @@
 import openpyxl as op
 
-filename = 'test.xlsx'
+filename = 'lanet.xlsx'
+subcategories_dict = {}
 
 wb = op.load_workbook(filename, data_only=True)
 sheet = wb.active
 
-max_row = sheet.max_row
+max_rows = sheet.max_row
 
-print(max_row)
+print(max_rows)
 
-print(sheet.cell(row=11, column=1).value)
+for i in range(2, max_rows+1):
+    sku = sheet.cell(row=i, column=2).value
+    subcategory = sheet.cell(row=i, column=1). value
+    if not sku:
+        continue
+
+    if subcategory not in subcategories_dict:
+        subcategories_dict[subcategory] = [sku]
+    else:
+        subcategories_dict[subcategory].append(sku)
+
+sorteddict = dict(sorted(subcategories_dict.items()))
+
+with open('subcategories.ini', 'w') as myfile:
+
+    for key, value in sorteddict.items():
+        string_values =', '.join(value)
+        string_to_write = key + ' = ' + string_values + '\n'
+        myfile.write(string_to_write)
